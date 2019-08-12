b863669f85bf60353cf0116d1d4842e14f343598
Ingo Fruend <mail@ingofruend.net>
2019-08-12 11:52:24 -0400

    folder templates

--- src/main/python/tmpl/process.py
+++ src/main/python/tmpl/process.py
@@ -1,4 +1,5 @@
 import os
+import glob
 from shutil import copyfile
 from tmpl.analyze import get_template_name
 
@@ -9,6 +10,10 @@ def copy_template(filename, template_folder):
     correct_filename = get_filename(filename)
     if os.path.isfile(template_name):
         copy_file_template(template_name, correct_filename)
+    elif os.path.isdir(template_name):
+        copy_directory_template(template_name, correct_filename)
+    else:
+        print('Doing nothing')
 
 
 def get_filename(filename):
@@ -19,3 +24,13 @@ def get_filename(filename):
 
 def copy_file_template(template_name, target_name):
     copyfile(template_name, target_name)
+
+
+def copy_directory_template(template_name, target_name):
+    filenames = glob.glob(os.path.join(template_name, '*'))
+    for fname in filenames:
+        base = os.path.basename(fname)
+        if base.startswith('main'):
+            copyfile(fname, target_name)
+        else:
+            os.symlink(fname, base)
