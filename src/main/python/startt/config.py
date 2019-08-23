a19f8e04ba8ebd1f19ad3bbb3a772cd8fcac51d5
Ingo Fruend <mail@ingofruend.net>
2019-08-23 14:59:06 -0400

    testing config stuff

--- src/main/python/startt/config.py
+++ src/main/python/startt/config.py
@@ -1,12 +1,17 @@
 import os
 import json
 
+
 def config(path):
     filename = os.path.join(path, 'config.json')
     if os.path.exists(filename):
         with open(filename) as f:
             out = json.load(f)
-            out.setdefault('default', 'link')
-            return out
+        if not set(out.values()).issubset({'link', 'copy'}):
+            raise ValueError('Invalid transfer option in config: {}'
+                             .format(set(out.values()) - {'link', 'copy'})
+                             )
+        out.setdefault('default', 'link')
+        return out
     else:
         return {'default': 'link'}
---
+++ src/unittest/python/config_tests.py
@@ -0,0 +1,61 @@
+from unittest import TestCase
+from unittest import mock
+
+from startt import config
+
+
+class TestConfig(TestCase):
+
+    def setUp(self):
+        self.patches = {
+            'exists': mock.patch('startt.config.os.path.exists'),
+            'open': mock.patch('startt.config.open'),
+            'json_load': mock.patch('startt.config.json.load'),
+        }
+        self.mocks = {
+            name: patch.start() for name, patch in self.patches.items()
+        }
+
+    def tearDown(self):
+        mock.patch.stopall()
+
+    def test_default_is_link(self):
+        self.mocks['exists'].return_value = False
+        cfg = config.config('ANY_PATH')
+        self.assertDictEqual({'default': 'link'}, cfg)
+
+    def test_default_is_set_if_read_from_file(self):
+        self.mocks['exists'].return_value = True
+        self.mocks['json_load'].return_value = {}
+
+        cfg = config.config('ANY_PATH')
+        self.assertIn('default', cfg)
+        self.assertEqual(cfg['default'], 'link')
+
+    def test_default_can_be_overwritten_from_file(self):
+        self.mocks['exists'].return_value = True
+        self.mocks['json_load'].return_value = {'default': 'copy'}
+
+        cfg = config.config('ANY_PATH')
+        self.assertIn('default', cfg)
+        self.assertEqual(cfg['default'], 'copy')
+
+    def test_filename_is_relative_to_project_path(self):
+        self.mocks['exists'].return_value = False
+        config.config('ANY_PATH')
+        self.mocks['exists'].assert_called_once_with('ANY_PATH/config.json')
+
+    def test_other_filenames_can_be_specified_separately(self):
+        self.mocks['exists'].return_value = True
+        self.mocks['json_load'].return_value = {'file.py': 'copy',
+                                                'other_file.txt': 'link'}
+        cfg = config.config('ANY_PATH')
+        self.assertDictEqual(cfg, {'default': 'link',
+                                   'file.py': 'copy',
+                                   'other_file.txt': 'link'})
+
+    def test_fail_if_invalid_transfer_options(self):
+        self.mocks['exists'].return_value = True
+        self.mocks['json_load'].return_value = {'file.py': 'invalid'}
+        with self.assertRaises(ValueError):
+            config.config('ANY_PATH')
